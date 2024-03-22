7abbf84fdbb79203ed15e1ece07d6a4906c6bb9f
Valsaa <valentin.jaouen@gmail.com>
2024-03-22 15:48:03 +0000

    client: create simple tcp client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,46 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
 #include <iostream>
 
-int main() {
-    std::cout << "I'm client !" << std::endl;
+int main(int argc, char *argv[]) {
+    const auto PORT{8080};
+    auto sock{0};
+    auto hello{"Hello from client"};
+
+    // Creating socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in srv_addr {};
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    srv_addr.sin_port = htons(PORT);
+
+    // Check if IP address is valid
+    if (srv_addr.sin_addr.s_addr == INADDR_NONE) {
+        perror("invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(sock, reinterpret_cast<sockaddr *>(&srv_addr),
+                sizeof(srv_addr)) < 0) {
+        perror("connection failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send message to server
+    send(sock, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+
+    // Close the socket
+    close(sock);
     return 0;
 }
